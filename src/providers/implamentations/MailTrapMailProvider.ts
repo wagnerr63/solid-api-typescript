0dc5d40b20c1cef2c1e4eb882eb87f9aedcc7d3c
wagnerribas <wagnerribas63@gmail.com>
2021-06-09 22:34:44 -0300

    MailTrapProvider e Controller working!

--- src/providers/implamentations/MailTrapMailProvider.ts
+++ src/providers/implamentations/MailTrapMailProvider.ts
@@ -1,11 +1,32 @@
 import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
 
 export class MailtrapMailProvider implements IMailProvider {
-    constructor(
-
-    ){}
+    private trasnporter: Mail;
+    constructor(){
+        this.trasnporter = nodemailer.createTransport({
+            host: "smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+              user: "87dc6410bddb8e",
+              pass: "f60c45f9aaca72"
+            }
+        });
+    }
 
     async sendMail(message: IMessage): Promise<void> {
-
+        await this.trasnporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        });
     }
 }
---
+++ src/useCases/createUser/index.ts
@@ -0,0 +1,16 @@
+import { MailtrapMailProvider } from "../../providers/implamentations/MailTrapMailProvider";
+import { PostgresUsersRepository } from "../../repositories/implamentations/PostgresUsersReposity";
+import { CreateUserController } from "./createUserController";
+import { CreateUserUseCase } from "./createUserUseCase";
+
+const mailtrapMailProvider = new MailtrapMailProvider();
+const postgresUsersRepository = new PostgresUsersRepository(); 
+
+const createUserUseCase = new CreateUserUseCase(
+    postgresUsersRepository, 
+    mailtrapMailProvider
+);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+export { createUserUseCase, createUserController}
